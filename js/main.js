104bf6eb5efb8e2230764a1785152729db6ad150
Klirik <dasoftweb@gmail.com>
2021-03-02 00:43:52 +0200

    3

--- js/main.js
+++ js/main.js
@@ -1,45 +1,44 @@
-const refs = {
-  daysRef: document.querySelector("[data-value='days']"),
-  hoursRef: document.querySelector("[data-value='hours']"),
-  minsRef: document.querySelector("[data-value='mins']"),
-  secsRef: document.querySelector("[data-value='secs']"),
-};
+class CountdownTimer {
+  constructor(settings) {
+    this.targetDate = settings.targetDate;
+    this.selectorRef = document.querySelector(settings.selector),
+    this.refs = {
+      daysRef: this.selectorRef.querySelector("[data-value='days']"),
+      hoursRef: this.selectorRef.querySelector("[data-value='hours']"),
+      minsRef: this.selectorRef.querySelector("[data-value='mins']"),
+      secsRef: this.selectorRef.querySelector("[data-value='secs']"),
+    };
+    this.init();
+  }
 
-const targetDate = new Date("Mar 7, 2021");
-
-const timer = {
-  start() {
+  init() {
     setInterval(() => {
-      const timeA = updateClockface(targetDate - Date.now());
-      setTime(timeA);
+      const timer = this.updateClockface(this.targetDate - Date.now());
+      this.render(timer);
     }, 1000);
-  },
-};
+  }
 
-timer.start();
+  updateClockface(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
+  }
 
-function updateClockface(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  return { days, hours, mins, secs };
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  render({ days, hours, mins, secs }) {
+    this.refs.daysRef.textContent = days;
+    this.refs.hoursRef.textContent = hours;
+    this.refs.minsRef.textContent = mins;
+    this.refs.secsRef.textContent = secs;
+  }
 }
 
-function pad(value) {
-  return String(value).padStart(2, "0");
-}
-
-function setTime({ days, hours, mins, secs }) {
-  refs.daysRef.textContent = days;
-  refs.hoursRef.textContent = hours;
-  refs.minsRef.textContent = mins;
-  refs.secsRef.textContent = secs;
-}
-
-// new CountdownTimer({
-//   selector: "#timer-1",
-//   targetDate: new Date("Mar 7, 2021"),
-// });
+new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Mar 7, 2021"),
+});
