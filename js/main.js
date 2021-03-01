cdabbf4792325d10ec651a3b01a82a4f7452d341
Klirik <dasoftweb@gmail.com>
2021-03-01 21:30:29 +0200

    init

--- js/main.js
+++ js/main.js
@@ -1,30 +1,45 @@
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+const refs = {
+  daysRef: document.querySelector("[data-value='days']"),
+  hoursRef: document.querySelector("[data-value='hours']"),
+  minsRef: document.querySelector("[data-value='mins']"),
+  secsRef: document.querySelector("[data-value='secs']"),
+};
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const targetDate = new Date("Mar 7, 2021");
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+const timer = {
+  start() {
+    setInterval(() => {
+      const timeA = updateClockface(targetDate - Date.now());
+      setTime(timeA);
+    }, 1000);
+  },
+};
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+timer.start();
 
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
-});
+function updateClockface(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(
+    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  );
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  return { days, hours, mins, secs };
+}
 
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+function setTime({ days, hours, mins, secs }) {
+  refs.daysRef.textContent = days;
+  refs.hoursRef.textContent = hours;
+  refs.minsRef.textContent = mins;
+  refs.secsRef.textContent = secs;
+}
+
+// new CountdownTimer({
+//   selector: "#timer-1",
+//   targetDate: new Date("Mar 7, 2021"),
+// });
